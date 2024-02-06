ecffd3c3d8a178cbe614600104af45a24de5ae9c
Nate Sire <nate.sire@fairwaymc.com>
2024-02-05 21:01:09 -0500

    rename

--- main.py
+++ main.py
@@ -9,19 +9,40 @@
 
 # pick any positive n from 1 to 9 inclusive
 # can try larger numbers too
-n = 3
+def formulaIfOdd(n, multiplyBy=3):
+    return multiplyBy * n + 1    
+
+def calculateConjecture(n, multBy=3):
+    print("Conjecture for " + str(n))
+    while(1):
+        if n == 1.0:
+            break
     
-def formulaIfOdd(n):
-    return 3 * n + 1    
-    
-while(1):
-    if n == 1.0:
-        break
-    
-    # if r is even then divide by 2
-    if n % 2 == 0.0:
-        n = n / 2
-        print(n)
-    else: 
-        n = formulaIfOdd(n)
-        print(n)
+        # if r is even then divide by 2
+        if n % 2 == 0.0:
+            n = n / 2
+            print(n)
+        else: 
+            n = formulaIfOdd(n, multBy)
+            print(n)
+    return n
+
+# start at, multiply * n + 1
+calculateConjecture(3)
+calculateConjecture(3, 3)
+calculateConjecture(1000, 3)
+
+# 8 works with any multiplier. it is the infinite number. God's number. twisted zero into plane of existance.
+
+# n=8 works for any k*n+1
+calculateConjecture(8, 1)
+calculateConjecture(8, 2)
+calculateConjecture(8, 3)
+calculateConjecture(8, 4)
+calculateConjecture(8, 5)
+calculateConjecture(8, 6)
+calculateConjecture(8, 7)
+calculateConjecture(8, 8)
+calculateConjecture(8, 9)
+calculateConjecture(8, 91)
+calculateConjecture(8, 912)
